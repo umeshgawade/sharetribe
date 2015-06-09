ab66a1196203e8d2b83051246ab0b772ace05b57
Mikko Koski <mikkokos@gmail.com>
2015-06-09 12:04:29 +0300

    Remove units from shapes without migrations
    - We shouldn't have units for shapes that do not have price enabled. However, probably due to earlier migrations, this has happened. This PR fixes the issue by removing units from shapes that do not have price enabled.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150528120717) do
+ActiveRecord::Schema.define(:version => 20150609085555) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
